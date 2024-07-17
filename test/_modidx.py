b44f21655d94df2c431184b2d988e1562e00e6e7
Daniel Sussman <sussman@bu.edu>
2024-07-17 10:54:37 -0400

    add another ipynb

--- test/_modidx.py
+++ test/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://dpmcsuss.github.io',
                 'git_url': 'https://github.com/dpmcsuss/test',
                 'lib_path': 'test'},
-  'syms': {'test.core': {'test.core.foo': ('core.html#foo', 'test/core.py')}}}
+  'syms': { 'test.core': {'test.core.foo': ('core.html#foo', 'test/core.py')},
+            'test.new': {'test.new.bar': ('new.html#bar', 'test/new.py')}}}
